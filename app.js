e18245282b635afc288a5c980b0b10b504d1bc8f
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2025-11-18 09:49:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js"
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js"
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDXbBUDJ-ZjKG36--lPh9Po_-7n41vXMB0",
-  authDomain: "huerta-2025.firebaseapp.com",
-  databaseURL: "https://huerta-2025-default-rtdb.firebaseio.com",
-  projectId: "huerta-2025",
-  storageBucket: "huerta-2025.firebasestorage.app",
-  messagingSenderId: "423826580167",
-  appId: "1:423826580167:web:9f43b9effffb895d7994e2"
-};
-
-const app = initializeApp(firebaseConfig)
-const db = getDatabase(app)
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js"; 
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+ // 🔧 CONFIGURACIÓN DE FIREBASE
+ const firebaseConfig = { apiKey: "AIzaSyCrB4xgCBRmpujDcvOOihSVZ6MIFOPE56M",
+  authDomain: "proyectoclase1-2025.firebaseapp.com", 
+  databaseURL: "https://huertamaxi2025-default-rtdb.firebaseio.com", 
+  projectId: "proyectoclase1-2025",
+   storageBucket: "proyectoclase1-2025.firebasestorage.app", 
+   messagingSenderId: "822869341917",
+    appId: "1:822869341917:web:8d4d76a6493df78963cd34" }; 
+    
+    const app = initializeApp(firebaseConfig); 
+  const db = getDatabase(app); 
 const parrafo = document.getElementById("datos")
 
-const refHuerta = ref(db, "Huerta")
+const refHuerta = ref(db, "/")
 
 onValue(refHuerta, (snapshot) => {
   const huerta = snapshot.val()
